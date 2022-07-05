eb0ae9cb38ef5ba8cb685c1e481d1d383328eee8
Ismael Cardoso Martins <est_imartins@banparanet.com.br>
2022-07-05 18:20:24 -0300

    API filmes 1.0

---
+++ API/Models/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; } = null;
+    }
+}
